abfaa4a1613f46771d18f68e24e08c6a46753c6a
Nate Hascup <nshascup@gmail.com>
2017-10-21 22:45:46 -0700

    ERRORS-uniquWords and longestWord not generating. EditTexts were not being updated in AnalyzerActivity&bindData; corrected them and ran into app crashing error; confirmed that uniqueWords worked, error was in longestWord; 2nd error resolved-TextAnalyzerUtil&getLongestWord returns string and dense human programmer had copy-pasted code from other setText lines, which were coded to handle INTs, changed String.format to correctly display data type and program is WAI

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
@@ -1,7 +1,9 @@
 package co.miniforge.corey.stringanalyzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +37,17 @@ public class AnalyzerActivity extends AppCompatActivity {
         locateViews();
 
         bindData();
+
+        backToInputButton = (Button) findViewById(R.id.backToInputButton);
+        backToInputButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AnalyzerActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     void locateViews() {
@@ -62,6 +75,8 @@ public class AnalyzerActivity extends AppCompatActivity {
         characterCount.setText(String.format("Character Count: %d", textUtil.getTextCharacterCount()));
         wordCount.setText(String.format("Word Count: %d", textUtil.getWordCount()));
         uniqueCharacters.setText(String.format("Unique Characters: %d", textUtil.getUniqueCharacters()));
+        uniqueWords.setText(String.format("Unique Words: %d", textUtil.getUniqueWords()));
+        longestWord.setText(String.format("Longest Word: " + textUtil.getLongestWord()));
         specialCharactersCount.setText(String.format("Special Characters: %d", textUtil.getSpecialCharacterCount()));
     }
 }
